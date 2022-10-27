bec5fc9f6c1d955446f5434914252ff262c3cd04
Luc van den Brand <lucvandenbrand@protonmail.com>
2022-10-27 12:44:42 +0200

    Added rust function example!

--- source/main.c
+++ source/main.c
@@ -1,79 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <gccore.h>
-#include <wiiuse/wpad.h>
+/*===========================================
+    NoNameNo
+    Simple Flat 3D cube
+============================================*/
+#include <grrlib.h>
 
-static void *xfb = NULL;
-static GXRModeObj *rmode = NULL;
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <wiiuse/wpad.h>
 
 extern void rust_function();
 
 void c_function() {
-	printf("Hello Rust!");
+	float a=0;
+    u32 col[3] = {0xFFFFFFFF, 0xAAAAAAFF, 0x666666FF};
+    int cubeZ=0;
+
+    GRRLIB_Init();
+    WPAD_Init();
+
+    GRRLIB_Settings.antialias = true;
+
+    GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
+    GRRLIB_Camera3dSettings(0.0f,0.0f,13.0f, 0,1,0, 0,0,0);
+
+    while(1) {
+        WPAD_ScanPads();
+        if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
+        if(WPAD_ButtonsHeld(0) & WPAD_BUTTON_A) cubeZ++;
+        if(WPAD_ButtonsHeld(0) & WPAD_BUTTON_B) cubeZ--;
+
+        GRRLIB_3dMode(0.1,1000,45,0,0);
+        GRRLIB_ObjectView(0,0,cubeZ, a,a*2,a*3,1,1,1);
+        GX_Begin(GX_QUADS, GX_VTXFMT0, 24);
+            GX_Position3f32(-1.0f,1.0f,-1.0f);
+            GX_Color1u32(col[0]);
+            GX_Position3f32(-1.0f,-1.0f,-1.0f);
+            GX_Color1u32(col[0]);
+            GX_Position3f32(1.0f,-1.0f,-1.0f);
+            GX_Color1u32(col[0]);
+            GX_Position3f32(1.0f,1.0f,-1.0f);
+            GX_Color1u32(col[0]);
+
+            GX_Position3f32(-1.0f,1.0f,1.0f);
+            GX_Color1u32(col[0]);
+            GX_Position3f32(-1.0f,-1.0f,1.0f);
+            GX_Color1u32(col[0]);
+            GX_Position3f32(1.0f,-1.0f,1.0f);
+            GX_Color1u32(col[0]);
+            GX_Position3f32(1.0f,1.0f,1.0f);
+            GX_Color1u32(col[0]);
+
+            GX_Position3f32(-1.0f,1.0f,1.0f);
+            GX_Color1u32(col[1]);
+            GX_Position3f32(1.0f,1.0f,1.0f);
+            GX_Color1u32(col[1]);
+            GX_Position3f32(1.0f,1.0f,-1.0f);
+            GX_Color1u32(col[1]);
+            GX_Position3f32(-1.0f,1.0f,-1.0f);
+            GX_Color1u32(col[1]);
+
+            GX_Position3f32(-1.0f,-1.0f,1.0f);
+            GX_Color1u32(col[1]);
+            GX_Position3f32(1.0f,-1.0f,1.0f);
+            GX_Color1u32(col[1]);
+            GX_Position3f32(1.0f,-1.0f,-1.0f);
+            GX_Color1u32(col[1]);
+            GX_Position3f32(-1.0f,-1.0f,-1.0f);
+            GX_Color1u32(col[1]);
+
+            GX_Position3f32(-1.0f,1.0f,1.0f);
+            GX_Color1u32(col[2]);
+            GX_Position3f32(-1.0f,1.0f,-1.0f);
+            GX_Color1u32(col[2]);
+            GX_Position3f32(-1.0f,-1.0f,-1.0f);
+            GX_Color1u32(col[2]);
+            GX_Position3f32(-1.0f,-1.0f,1.0f);
+            GX_Color1u32(col[2]);
+
+            GX_Position3f32(1.0f,1.0f,1.0f);
+            GX_Color1u32(col[2]);
+            GX_Position3f32(1.0f,1.0f,-1.0f);
+            GX_Color1u32(col[2]);
+            GX_Position3f32(1.0f,-1.0f,-1.0f);
+            GX_Color1u32(col[2]);
+            GX_Position3f32(1.0f,-1.0f,1.0f);
+            GX_Color1u32(col[2]);
+        GX_End();
+        a+=0.5f;
+
+        GRRLIB_Render();
+    }
+    GRRLIB_Exit(); // Be a good boy, clear the memory allocated by GRRLIB
 }
 
-//---------------------------------------------------------------------------------
-int main(int argc, char **argv) {
-//---------------------------------------------------------------------------------
+int main() {
 	rust_function();
-
-	// Initialise the video system
-	VIDEO_Init();
-
-	// This function initialises the attached controllers
-	WPAD_Init();
-
-	// Obtain the preferred video mode from the system
-	// This will correspond to the settings in the Wii menu
-	rmode = VIDEO_GetPreferredMode(NULL);
-
-	// Allocate memory for the display in the uncached region
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-
-	// Initialise the console, required for printf
-	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-
-	// Set up the video registers with the chosen mode
-	VIDEO_Configure(rmode);
-
-	// Tell the video hardware where our display memory is
-	VIDEO_SetNextFramebuffer(xfb);
-
-	// Make the display visible
-	VIDEO_SetBlack(FALSE);
-
-	// Flush the video register changes to the hardware
-	VIDEO_Flush();
-
-	// Wait for Video setup to complete
-	VIDEO_WaitVSync();
-	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-
-
-	// The console understands VT terminal escape codes
-	// This positions the cursor on row 2, column 0
-	// we can use variables for this with format codes too
-	// e.g. printf ("\x1b[%d;%dH", row, column );
-	printf("\x1b[2;0H");
-
-
-	
-
-	while(1) {
-
-		// Call WPAD_ScanPads each loop, this reads the latest controller states
-		WPAD_ScanPads();
-
-		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
-		// this is a "one shot" state which will not fire again until the button has been released
-		u32 pressed = WPAD_ButtonsDown(0);
-
-		// We return to the launcher application via exit
-		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
-
-		// Wait for the next frame
-		VIDEO_WaitVSync();
-	}
-
-	return 0;
+    exit(0);
 }
