7d2994f3561a56aba123b9af760a01fb075e3092
Thijs van der Knaap <knaap@live.com>
2022-12-27 23:14:42 +0100

    merged from Wiebes fixes, and removed error of renderer going out of scope

--- app/src/main.rs
+++ app/src/main.rs
@@ -52,38 +52,36 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
 fn main_game() -> isize {
     println!("Hello Rust!");
     register_power_callback();
-    {
-        // Setup the wiimote
-        Input::init(ControllerType::Wii);
-        let wii_mote = Input::new(ControllerType::Wii, ControllerPort::One);
-        wii_mote
-            .as_wpad()
-            .set_data_format(WPadDataFormat::ButtonsAccelIR);
 
-        // Setup the ECS environment.
-        let mut world = World::new();
-        batch_spawn_entities(&mut world, 5);
-        let mut velocity_query = PreparedQuery::<&mut Velocity>::default();
-        let mut all_query = PreparedQuery::<(&mut Position, &mut Velocity)>::default();
+    // Setup the wiimote
+    Input::init(ControllerType::Wii);
+    let wii_mote = Input::new(ControllerType::Wii, ControllerPort::One);
+    wii_mote
+        .as_wpad()
+        .set_data_format(WPadDataFormat::ButtonsAccelIR);
 
-        // Kickstart main loop.
-        let renderer = Renderer::new();
-        while KEEP_RUNNING.load(Ordering::SeqCst) {
-            Input::update(ControllerType::Wii);
-            if wii_mote.is_button_down(Button::Home) {
-                break;
-            }
-            if wii_mote.is_button_down(Button::One) {
-                system_shake_wii(&mut world, &mut velocity_query);
-            }
-            system_bounce_bounds(&mut world, &mut all_query);
-            system_integrate_motion(&mut world, &mut all_query);
+    // Setup the ECS environment.
+    let mut world = World::new();
+    batch_spawn_entities(&mut world, 5);
+    let mut velocity_query = PreparedQuery::<&mut Velocity>::default();
+    let mut all_query = PreparedQuery::<(&mut Position, &mut Velocity)>::default();
 
-            renderer.render_world(&world);
+    // Kickstart main loop.
+    let renderer = Renderer::new();
+    while KEEP_RUNNING.load(Ordering::SeqCst) {
+        Input::update(ControllerType::Wii);
+        if wii_mote.is_button_down(Button::Home) {
+            break;
         }
+        if wii_mote.is_button_down(Button::One) {
+            system_shake_wii(&mut world, &mut velocity_query);
+        }
+        system_bounce_bounds(&mut world, &mut all_query);
+        system_integrate_motion(&mut world, &mut all_query);
+
+        renderer.render_world(&world);
     }
     shutdown();
-    0
 }
 
 /// Registers the power callback,
