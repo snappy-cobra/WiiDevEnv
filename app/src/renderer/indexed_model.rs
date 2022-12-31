e2c01b1b44220779521864d6331f2e79e2e6dcee
Luc van den Brand <lucvandenbrand@protonmail.com>
2022-12-31 12:46:08 +0100

    Refdactored indexedmodel.

--- app/src/renderer/indexed_model.rs
+++ app/src/renderer/indexed_model.rs
@@ -21,34 +21,50 @@ pub const BYTE_SIZE_TEX_COORD: usize = core::mem::size_of::<f32>() * SIZE_TEX_CO
 
 /**
  * Implementation of the indexed model.
+ * This is done by filling memo tables whose keys are known indexable vertex attributes that we have seen before,
+ * and whose values are indexes into an array containing these vertex attributes.
  */
 impl IndexedModel {
     /**
      * Turn a model into its indexed equivalent.
-     *
-     * This is done by filling memotables whose keys are known indexable vertex attributes that we have seen before,
-     * and whose values are indexes into an array containing these vertex attributes.
      */
     pub fn new(obj_data: &Obj) -> IndexedModel {
-        let mut vertex_memo: BTreeMap<Index, u16> = BTreeMap::new();
-        let mut tex_coord_memo: BTreeMap<Index, u16> = BTreeMap::new();
-        let mut positions = Vec::new();
-        let mut tex_coords: Vec<f32> = Vec::new();
-        
-        // Add vertex positions
-        let position_indices = obj_data
-            .vertices()
-            .map(|vertex| {
-                let vertex_id = vertex.position_index();
-                *vertex_memo.entry(vertex_id).or_insert_with(|| {
-                    let index = u16::try_from(positions.len() / SIZE_POSITION).unwrap();
-                    positions.extend(vertex.position());
-                    index
-                })
-            })
-            .collect();
+        let (positions, position_indices) = Self::index_positions(obj_data);
+        let (tex_coords, tex_coord_indices) = Self::index_tex_coords(obj_data);
+        IndexedModel {
+            positions,
+            position_indices,
+            tex_coords,
+            tex_coord_indices,
+        }
+    }
 
-        // Add tex coords
+    /**
+     * Generated indexed position data from the given wavefront object.
+     */
+    fn index_positions(obj_data: &Obj) -> (Vec<f32>, Vec<u16>) {
+        let mut vertex_memo: BTreeMap<Index, u16> = BTreeMap::new();
+        let mut positions = Vec::new();
+        let position_indices = obj_data
+        .vertices()
+        .map(|vertex| {
+            let vertex_id = vertex.position_index();
+            *vertex_memo.entry(vertex_id).or_insert_with(|| {
+                let index = u16::try_from(positions.len() / SIZE_POSITION).unwrap();
+                positions.extend(vertex.position());
+                index
+            })
+        })
+        .collect();
+        return (positions, position_indices);
+    }
+
+    /**
+     * Generated indexed texture coordinate data from the given wavefront object.
+     */
+    fn index_tex_coords(obj_data: &Obj) -> (Vec<f32>, Vec<u16>) {
+        let mut tex_coord_memo: BTreeMap<Index, u16> = BTreeMap::new();
+        let mut tex_coords: Vec<f32> = Vec::new();
         let tex_coord_indices = obj_data
             .vertices()
             .map(|vertex| {
@@ -63,12 +79,6 @@ impl IndexedModel {
                 })
             })
             .collect();
-
-        IndexedModel {
-            positions,
-            position_indices,
-            tex_coords,
-            tex_coord_indices,
-        }
+        return (tex_coords, tex_coord_indices);
     }
 }
