6ba669385fe289f5be2c02a93487c897da472dc1
Luc van den Brand <lucvandenbrand@protonmail.com>
2023-01-22 11:35:14 +0100

    Implemented display list interface.

--- app/src/renderer/display_cache.rs
+++ app/src/renderer/display_cache.rs
@@ -1,12 +1,76 @@
-use libc::c_void;
+use grrustlib::{GX_BeginDispList, GX_EndDispList, DCInvalidateRange};
+use libc::{c_void, memalign, free, realloc};
 
+const ALIGN_SIZE: usize = 32;
+const DEFAULT_LIST_SIZE: usize = 1024;
+
+/**
+ * Caches display lists based on the textured model name.
+ */
 pub struct DisplayCache {
     display_list_map: BTreeMap<TexturedModelName, c_void>,
 }
 
-pub impl DisplayCache {
-    pub fn set_display_list(&self, key: &TexturedModelName) -> Option<c_void> {
-        self.display_list_map.get(key)
+impl DisplayCache {
+    pub fn get_display_list(&self, key: &TexturedModelName) -> DisplayList {
+        match self.display_list_map.get(key) {
+            Some(display_list) => display_list,
+            None => {
+                let display_list =  DisplayList::new();
+                self.display_list_map.insert(key, display_list);
+                display_list
+            } 
+        }
     }
-    pub 
+}
+
+/**
+ * Interface for creating a GX Display list.
+ */
+pub struct DisplayList {
+    initialized: bool,
+    list_size: usize,
+    gx_list: *mut c_void
+}
+
+impl DisplayList {
+    pub fn new() -> Self {
+        let list_size = ALIGN_SIZE * DEFAULT_LIST_SIZE;
+        unsafe {
+            Self {
+                initialized: false,
+                list_size,
+                gx_list: memalign(ALIGN_SIZE, list_size)
+            }
+        }
+    }
+
+    pub fn is_initialized(&self) -> bool {
+        self.initialized
+    }
+
+    pub fn open(&mut self) {
+        unsafe {
+            DCInvalidateRange(self.gx_list, self.list_size);
+            GX_BeginDispList(self.gx_list, self.list_size);
+        }
+        self.initialized = false;
+    }
+
+    pub fn close(&mut self) {
+        unsafe {
+            self.list_size = GX_EndDispList();
+            realloc(self.gx_list, self.list_size);
+        }
+        self.initialized = true;
+    }
+}
+
+impl Drop for DisplayList {
+    /// Cleanup the display list
+    fn drop(&mut self) {
+        unsafe {
+            free(self.gx_list);
+        }
+    }
 }
