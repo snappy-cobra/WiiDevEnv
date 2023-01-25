2866323bb30f782c3141c576e11896c54466d4fb
Luc van den Brand <lucvandenbrand@protonmail.com>
2023-01-25 16:11:18 +0100

    Added integration test.

---
+++ app/src/target_tests/test_display_cache.rs
@@ -0,0 +1,27 @@
+use crate::rendering::display_cache::DisplayCache;
+use crate::rendering::textured_model::TexturedModelName;
+use hashbrown::HashMap;
+
+/**
+ * Test if the model factory behaves as expected.
+ */
+pub fn test_display_cache(tests: &mut HashMap<&'static str, fn()>) {
+    tests.insert("Test creating a display cache list.", test_create_cache_list);
+}
+
+/**
+ * Test if we can create a display cache
+ */
+fn test_create_cache_list() {
+    let mut display_cache = DisplayCache::new();
+    let list = display_cache.get_display_list(&TexturedModelName::Cube);
+    assert!(!list.is_initialized());
+    list.open();
+    assert!(!list.is_initialized());
+    list.close();
+    assert!(list.is_initialized());
+    list.open();
+    assert!(!list.is_initialized());
+    list.close();
+    assert!(list.is_initialized());
+}
