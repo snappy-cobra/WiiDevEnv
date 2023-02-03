0ae518e613583f7a38387e0cb88fac5d61711c64
Thijs van der Knaap <knaap@live.com>
2023-02-03 18:10:17 +0100

    fixed unit test

--- app/gamelib/src/plot.rs
+++ app/gamelib/src/plot.rs
@@ -1,11 +1,5 @@
 use alloc::format;
-#[cfg(feature = "wii")]
-use ogc_rs::prelude::Vec;
-#[cfg(feature = "wii")]
-use ogc_rs::prelude::vec;
-#[cfg(not(feature = "wii"))]
 use alloc::vec::Vec;
-#[cfg(not(feature = "wii"))]
 use alloc::vec;
 use alloc::string::String;
 use crate::alloc::string::ToString;
